3fb58a04ab75320acc2bad982329c6be062dc282
Prof2807 <proffessors2807@gmail.com>
2026-02-02 18:58:17 +0530

    ha

--- final.js
+++ final.js
@@ -2,17 +2,17 @@ let yo ='yo'
 
 console.log("Keep")
 
-console.log("This will stay") 
 
-console.log("This will stay") 
+
+
 
 // yo
 
-// asd debug spohdoi soidh sodfh sdho  
+// asd debug   
 
 let num = 0 
 
 console.log("Keep")
-console.log("Remove me") // remove
-console.log("Also rm me") 
+
+
  //com
